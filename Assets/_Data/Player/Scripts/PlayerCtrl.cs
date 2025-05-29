44493742982d914f8986a0d4e84670fd84aa0892
MAn124 <115962944+MAn124@users.noreply.github.com>
2025-05-29 19:41:17 +0700

    Player idle, moving animation, look mouse position

--- Assets/_Data/Player/Scripts/PlayerCtrl.cs
+++ Assets/_Data/Player/Scripts/PlayerCtrl.cs
@@ -3,5 +3,24 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class PlayerCtrl : BaseMonoBehaviour
 {
- 
+    [SerializeField] protected PlayerMovement playerMovement;
+    [SerializeField] protected Animator animator;
+
+    public Animator Animator => animator;
+    protected override void LoadComponent()
+    {
+        base.LoadComponent();
+        this.LoadMovement();
+        this.LoadAnimation();
+    }
+    protected virtual void LoadMovement()
+    {
+        if (playerMovement != null) return;
+        this.playerMovement = GetComponentInChildren<PlayerMovement>();
+    }
+    protected virtual void LoadAnimation()
+    {
+        if (this.animator != null) return;
+        this.animator = GetComponent<Animator>();
+    }
 }
--- Assets/_Data/Player/Scripts/PlayerMovement.cs
+++ Assets/_Data/Player/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : BaseMonoBehaviour
@@ -6,25 +7,46 @@ public class PlayerMovement : BaseMonoBehaviour
     [SerializeField] protected PlayerCtrl playerCtrl;
     [SerializeField] protected float moveSpeed = 5f;
     protected Vector3 moveInput;
+    protected bool isMoving = false;
+    [SerializeField] protected SpriteRenderer spriteRenderer;
     protected void FixedUpdate()
     {
         this.HandleMoving();
+        this.LookMousePos();
     }
     protected override void LoadComponent()
     {
         base.LoadComponent();
         this.LoadPlayerCtrl();
+        this.LoadSprite();
     }
     protected virtual void LoadPlayerCtrl()
     {
         if (this.playerCtrl != null) return;
         this.playerCtrl = GetComponentInParent<PlayerCtrl>();
     }
+    protected virtual void LoadSprite()
+    {
+        if(this.spriteRenderer != null) return;
+        this.spriteRenderer = transform.parent.GetComponentInChildren<SpriteRenderer>();
+    }
     protected virtual void HandleMoving()
     {
-        moveInput.x = Input.GetAxis("Horizontal");
-        moveInput.y = Input.GetAxis("Vertical");
-
-        transform.parent.position += moveInput * moveSpeed * Time.fixedDeltaTime;
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        transform.parent.position += moveSpeed * Time.fixedDeltaTime * moveInput;
+        if (moveInput.sqrMagnitude > 0.01f) {
+            this.isMoving = true;
+            this.playerCtrl.Animator.SetBool("isMoving", isMoving); }
+        else {
+            this.isMoving = false;
+            this.playerCtrl.Animator.SetBool("isMoving", isMoving); }
+    }
+    protected virtual void LookMousePos()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("mousePos: " + mousePos);
+        if(mousePos.x < transform.position.x) spriteRenderer.flipX = false;
+        else spriteRenderer.flipX = true;
     }
 }
